05cf9736a4b178cf446406a154c8756e3196d84d
Jeb Davis <jebemery@gmail.com>
2025-09-16 11:02:28 -0600

    Sounds/music added; AI added; other tweaks

--- Pirate Pong/Scripts/ball.gd
+++ Pirate Pong/Scripts/ball.gd
@@ -16,14 +16,16 @@ func _physics_process(delta):
 		var newVector:Vector2 = moveVector
 		if collision:
 			if collision.get_collider() is CharacterBody2D:
+				$"../AudioStreamPlayer3".play()
 				var colPos = collision.get_position()
 				var otherPos = collision.get_collider().get_position()
-				newVector = Vector2(cos(deg_to_rad(65*(colPos[1]-otherPos[1])/60)),sin(deg_to_rad(65*(colPos[1]-otherPos[1])/60)))
+				newVector = Vector2(cos(deg_to_rad(30*(colPos[1]-otherPos[1])/60)),sin(deg_to_rad(65*(colPos[1]-otherPos[1])/60)))
 				if position[0] > 540:
 					newVector = Vector2(newVector[0]*-1,newVector[1])
 			elif collision.get_collider() is TopWall:
 				newVector = moveVector.bounce(collision.get_normal())
 			else:
+				$"../AudioStreamPlayer2".play()
 				if collision.get_position().x < 960:
 					pointScored.emit("left")
 				else:
--- Pirate Pong/Scripts/paddle.gd
+++ Pirate Pong/Scripts/paddle.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 class_name Paddle
 
-enum PADDLE_CONTROLS {UPDOWN,LEFTRIGHT}
+enum PADDLE_CONTROLS {UPDOWN,LEFTRIGHT,BOT}
 @export var controls:PADDLE_CONTROLS
 @export var speed:float
 
@@ -13,6 +13,15 @@ func getInput():
 			inputVal = Input.get_axis("ui_up","ui_down")
 		PADDLE_CONTROLS.LEFTRIGHT:
 			inputVal = Input.get_axis("ui_left","ui_right")
+		PADDLE_CONTROLS.BOT:
+			if $"../Ball".global_position.y <= self.global_position.y:
+				inputVal = -1.0
+				if $"../Ball".global_position.x <self.global_position.x:
+					inputVal = 1.0
+			elif $"../Ball".global_position.y > self.global_position.y:
+				inputVal = 1.0
+				if $"../Ball".global_position.x <self.global_position.x:
+					inputVal = -1.0
 	velocity = Vector2(0,speed*inputVal)
 
 func _physics_process(delta):
